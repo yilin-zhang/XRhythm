Fix the data overflow bug, refactor.

lstm_model.py
@@ -8,53 +8,14 @@ from keras.layers import LSTM
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
 
 # Internal import
 from midi_data import MidiData
-from utils import get_file_path, phrase_to_multihot
+from utils import get_file_path, phrase_to_multihot, gen_batch
 
 # import constants
 from utils import DATASET_PATH, LENGTH_LIMIT
 from utils import INTERVAL_RANGE, DURATION_RANGE, REST_RANGE
-
-
-# Import dataset
-def gen_batch(dataset_path, n_steps, batch_size):
-    ''' Generate batch data from given dataset.
-    Args:
-    - dataset_path: The path to the dataset.
-    - n_steps: The steps of RNN.
-    - batch_size: batch size.
-
-    Returns:
-    - X_batch: The batch dataset as inputs.
-    - y_batch: The batch dataset as targets.
-    '''
-    X_batch = []
-    y_batch = []
-    for data_path, data_file in get_file_path(dataset_path, '.pkl'):
-        with open(data_path, 'rb') as f:
-            phrase_data = pickle.load(f)
-        for phrase in phrase_data:
-            n_slice = 0
-            # TODO In this case, some phrase fragments will be abandoned.
-            # eg. the phrase length is 25, then 0~20 can be reserved,
-            # but 21~24 will be abandoned.
-            # There might be some better solutions.
-            while (n_slice + 1) * n_steps + 1 <= phrase.__len__():
-                if X_batch.__len__() == batch_size:
-                    yield X_batch, y_batch
-                    X_batch = []
-                    y_batch = []
-                X_batch.append(
-                    phrase_to_multihot(
-                        phrase[n_slice * n_steps:(n_slice + 1) * n_steps]))
-                y_batch.append(
-                    phrase_to_multihot(phrase[n_slice * n_steps +
-                                              1:(n_slice + 1) * n_steps + 1]))
-                n_slice += 1
-
 
 # Model parameters
 n_steps = LENGTH_LIMIT
@@ -71,9 +32,6 @@ raw_target = [(i + 5) for i in range(100)]
 
 data = np.array(raw_Data, dtype=float)
 target = np.array(raw_target, dtype=float)
-
-data.shape
-target.shape
 
 x_train, x_test, y_train, y_test = train_test_split(
     data, target, test_size=0.2, random_state=4)

midi_data.py
@@ -44,8 +44,8 @@ class MidiData():
         '''Obtain instruments'''
         return self.pm.instruments
 
-    def write(self, midi_path):
-        self.pm.write(midi_path)
+    def write(self, path):
+        self.pm.write(path)
 
     def drop_drum(self):
         ''' Drop drum instruments in the PrettyMIDI object. The modification
@@ -218,7 +218,7 @@ class MidiData():
         monophony.
 
         Return:
-        - array_notes: A list that contains all the notes, which are converted
+        - note_list: A list that contains all the notes, which are converted
         to numpy arrays. The order: (interval, duartion, rest).
         '''
 
@@ -235,12 +235,15 @@ class MidiData():
         sec_unit = sec_per_beat * res_coef  # the time unit for quantization
 
         # all the elements are numpy arrays
-        array_notes = []
+        note_list = []
 
         last_end = 0
         last_pitch = 0
         for note in notes:
             # TODO make sure use the smallest unit to save space
+            # Notice that I have also trimmed the data to prevent overflow,
+            # which means if I need to change the dtype, I also have to change
+            # the trimming rule.
             # (interval, duartion, rest)
             # Note that in utils.py, function multihot_to_note uses np.int8 too.
             current_array = np.zeros(3, dtype=np.int8)
@@ -249,28 +252,40 @@ class MidiData():
             start = note.start
             end = note.end
             duration = round((end - start) / sec_unit)
+            # Trim the data to prevent overflow
+            if duration >= 128:
+                duration = 127
             current_array[1] = duration
 
             if last_end == 0:
                 # get the interval
                 interval = 0
                 current_array[0] = interval
-                # add current note to array_notes
-                array_notes.append(current_array)
+                # add current note to note_list
+                note_list.append(current_array)
             else:
                 # get the interval
                 interval = note.pitch - last_pitch
+                # Trim the data to prevent overflow
+                if interval >= 128:
+                    interval = 127
+                elif interval < -128:
+                    intervao = -128
                 current_array[0] = interval
+
                 # get the last note's rest
                 rest = round((note.start - last_end) / sec_unit)
-                array_notes[-1][2] = rest
-                # add current note to array_notes
-                array_notes.append(current_array)
+                # Trim the data to prevent overflow
+                if rest >= 128:
+                    rest = 127
+                note_list[-1][2] = rest
+                # add current note to note_list
+                note_list.append(current_array)
 
             last_pitch = note.pitch
             last_end = note.end
 
-        return array_notes
+        return note_list
 
     def note_to_array(self):
         instrument_list = []
@@ -294,6 +309,7 @@ class MidiData():
         note_list = copy.deepcopy(note_list)
 
         pm_melody = pretty_midi.PrettyMIDI()
+        # TODO The program name can be changed to piano.
         pm_melody_program = pretty_midi.instrument_name_to_program('Cello')
         pm_melody_instrument = pretty_midi.Instrument(
             program=pm_melody_program)

utils.py
@@ -4,6 +4,7 @@
 import os
 import copy
 import numpy as np
+import pickle
 
 RAW_MIDI_PATH = './raw_midi'
 PROCESSED_MIDI_PATH = './processed_midi'
@@ -37,7 +38,7 @@ def get_file_path(directory, suffix):
                 continue
 
             path = root + "/" + file_name
-        yield path, file_name
+            yield path, file_name
 
 
 def get_phrases(note_list):
@@ -135,14 +136,8 @@ def phrase_to_multihot(phrase):
     Return:
     - multihot_note: An multi-hot array, which is an `np.array` object.
     '''
-
-    multihot_phrase = copy.deepcopy(phrase)
-    idx = 0
-    for note in phrase:
-        multihot_note = note_to_multihot(note)
-        multihot_phrase[idx] = multihot_note
-        idx += 1
-    return multihot_phrase
+    multihot_note = list(map(note_to_multihot, phrase))
+    return multihot_note
 
 
 def multihot_to_note(multihot_note):
@@ -170,3 +165,41 @@ def multihot_to_note(multihot_note):
     note[2] = rest
 
     return note
+
+
+# Import dataset
+def gen_batch(dataset_path, n_steps, batch_size):
+    ''' Generate batch data from given dataset.
+    Args:
+    - dataset_path: The path to the dataset.
+    - n_steps: The steps of RNN.
+    - batch_size: batch size.
+
+    Returns:
+    - X_batch: The batch dataset as inputs.
+    - y_batch: The batch dataset as targets.
+    '''
+    X_batch = []
+    y_batch = []
+    for data_path, data_file in get_file_path(dataset_path, '.pkl'):
+        print(data_path)
+        with open(data_path, 'rb') as f:
+            phrase_data = pickle.load(f)
+        for phrase in phrase_data:
+            n_slice = 0
+            # TODO In this case, some phrase fragments will be abandoned.
+            # eg. the phrase length is 25, then 0~20 can be reserved,
+            # but 21~24 will be abandoned.
+            # There might be some better solutions.
+            while (n_slice + 1) * n_steps + 1 <= phrase.__len__():
+                if X_batch.__len__() == batch_size:
+                    yield X_batch, y_batch
+                    X_batch = []
+                    y_batch = []
+                X_batch.append(
+                    phrase_to_multihot(
+                        phrase[n_slice * n_steps:(n_slice + 1) * n_steps]))
+                y_batch.append(
+                    phrase_to_multihot(phrase[n_slice * n_steps +
+                                              1:(n_slice + 1) * n_steps + 1]))
+                n_slice += 1