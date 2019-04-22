d02c94ec333f985c23204a55e965193053ffc576
Yilin Zhang <yzhang0129@outlook.com>
2019-04-22 17:03:31 +0800

    Add a function to create three subfolders to split dataset.

--- get_dataset.py
+++ get_dataset.py
@@ -5,6 +5,7 @@ from midi_data import MidiData
 from utils import get_file_path, get_phrases
 import pickle
 import os
+import random
 
 from utils import PROCESSED_MIDI_PATH, LENGTH_LIMIT, DATASET_PATH
 
@@ -23,31 +24,80 @@ def dump_phrase_data(phrase_data, data_num, path):
         pickle.dump(phrase_data, f)
 
 
-# create directory if it does not exist.
-if not os.path.exists(DATASET_PATH):
-    os.makedirs(DATASET_PATH)
+def construct_dataset(path, proportion):
+    ''' Construct dataset by creating subfolders: training, validation, test.
+    Args:
+    - path: The path to the exist dataset folder.
+    - proportion: The proportion of training set, validation set and test set.
+    It can be a list or a tuple, which contains three numbers. The proportion
+    of these three numbers is the proportion of the three sets.
+    '''
 
-# process all the midi files.
-data_num = 0
-phrase_data = []
-for midi_path, midi_file in get_file_path(PROCESSED_MIDI_PATH, '.mid'):
-    try:
-        midi = MidiData(midi_path, res=1 / 16)
-    except:
-        print('corrupted: ' + midi_path)
-        continue
+    # Set random seed
+    random.seed(42)
 
-    instrument_list = midi.note_to_array()
+    # Obtain file list
+    file_list = list(os.walk(path))[0][2]
+    random.shuffle(file_list)
 
-    for note_list in instrument_list:
-        phrases = get_phrases(note_list)
-        for phrase in phrases:
-            if phrase.__len__() < LENGTH_LIMIT + 1:
-                continue
-            else:
-                phrase_data.append(phrase)
+    # Calculate three proportions
+    prop_sum = sum(proportion)
+    train_prop = proportion[0] / prop_sum
+    val_prop = proportion[1] / prop_sum
 
-    if phrase_data.__len__() >= 1000:
-        dump_phrase_data(phrase_data, data_num, DATASET_PATH)
-        data_num += 1
-        phrase_data = []
+    # obtain the number of
+    n_files = file_list.__len__()
+    n_train = round(n_files * train_prop)
+    n_val = round(n_files * val_prop)
+
+    # create directory if it does not exist.
+    if not os.path.exists(path + '/training'):
+        os.makedirs(path + '/training')
+    if not os.path.exists(path + '/validation'):
+        os.makedirs(path + '/validation')
+    if not os.path.exists(path + '/test'):
+        os.makedirs(path + '/test')
+
+    idx = 0
+    for file_name in file_list:
+        if idx < n_train:
+            os.rename(path + '/' + file_name, path + '/training/' + file_name)
+        elif idx >= n_train and idx < n_train + n_val:
+            os.rename(path + '/' + file_name,
+                      path + '/validation/' + file_name)
+        else:
+            os.rename(path + '/' + file_name, path + '/test/' + file_name)
+        idx += 1
+
+
+if __name__ == '__main__':
+    # create directory if it does not exist.
+    if not os.path.exists(DATASET_PATH):
+        os.makedirs(DATASET_PATH)
+
+    # process all the midi files.
+    data_num = 0
+    phrase_data = []
+    for midi_path, midi_file in get_file_path(PROCESSED_MIDI_PATH, '.mid'):
+        try:
+            midi = MidiData(midi_path, res=1 / 16)
+        except:
+            print('corrupted: ' + midi_path)
+            continue
+
+        instrument_list = midi.note_to_array()
+
+        for note_list in instrument_list:
+            phrases = get_phrases(note_list)
+            for phrase in phrases:
+                if phrase.__len__() < LENGTH_LIMIT + 1:
+                    continue
+                else:
+                    phrase_data.append(phrase)
+
+        if phrase_data.__len__() >= 1000:
+            dump_phrase_data(phrase_data, data_num, DATASET_PATH)
+            data_num += 1
+            phrase_data = []
+
+    construct_dataset(DATASET_PATH, (0.7, 0.15, 0.15))
