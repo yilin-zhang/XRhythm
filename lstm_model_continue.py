a3abb07625bc83a0a01a7ec715b1ba933d01bc24
Yilin Zhang <yzhang0129@outlook.com>
2019-05-22 22:48:30 +0800

    Change dropout rate to 0.5 when training drum music.

--- lstm_model_continue.py
+++ lstm_model_continue.py
@@ -2,7 +2,9 @@
 # Author: Yilin Zhang
 
 # External imports
-from keras.models import load_model
+from keras.models import Model, load_model
+from keras.layers import Input, Dense, LSTM, Dropout, LeakyReLU
+from keras.optimizers import Adadelta
 from keras.callbacks import TensorBoard, ModelCheckpoint
 
 # Internal imports
@@ -18,8 +20,10 @@ n_inputs = INTERVAL_RANGE + DURATION_RANGE + REST_RANGE
 n_outputs = DURATION_RANGE + REST_RANGE
 
 # Changeable model parameters (hyper parameters)
+n_neurons = 256
 batch_size = 5
 n_epochs = 100
+dropout_rate = 0.5
 initial_epoch = 50
 
 # These variables are related to batch size.
@@ -35,6 +39,42 @@ valid_path = XADRUM_DATASET_PATH + '/valid'
 
 MODEL_PATH = './models/201905030320/lstm_model.h5'
 model = load_model(MODEL_PATH)
+
+# Construct New Model
+model_input = Input(shape=(n_steps, n_inputs))
+x = LSTM(n_neurons, activation='linear', return_sequences=True)(model_input)
+x = LeakyReLU()(x)
+x = Dropout(dropout_rate)(x)
+x = LSTM(n_neurons, activation='linear', return_sequences=True)(x)
+x = LeakyReLU()(x)
+x = Dropout(dropout_rate)(x)
+x = LSTM(n_neurons, activation='linear', return_sequences=True)(x)
+x = LeakyReLU()(x)
+x = Dropout(dropout_rate)(x)
+x = LSTM(n_neurons, activation='linear', return_sequences=True)(x)
+x = LeakyReLU()(x)
+x = Dropout(dropout_rate)(x)
+duration_output = Dense(
+    DURATION_RANGE, activation='softmax', name='duration_output')(x)
+rest_output = Dense(REST_RANGE, activation='softmax', name='rest_output')(x)
+
+new_model = Model(inputs=model_input, outputs=[duration_output, rest_output])
+
+# Copy all the weights to the new model
+for new_layer, layer in zip(new_model.layers[1:], model.layers[1:]):
+    new_layer.set_weights(layer.get_weights())
+
+new_model.compile(
+    optimizer=Adadelta(),
+    loss={
+        'duration_output': 'categorical_crossentropy',
+        'rest_output': 'categorical_crossentropy'
+    },
+    loss_weights={
+        'duration_output': 0.5,
+        'rest_output': 0.5
+    },
+    metrics=['accuracy'])
 
 # Set tensorboard callback
 tb_callback = TensorBoard(log_dir='./logs', batch_size=batch_size)
